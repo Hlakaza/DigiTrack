26b1f036d558f298f03f5568ee20c1e74980ab2e
Hlakaza <nhlalucky9@gmail.com>
2018-10-13 13:38:37 +0200

    refactor(header): completed header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+
+const routes = [
+  // { path: 'add', component: AddComponent }
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +16,14 @@ import { HeaderComponent } from './components/header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule,
+    RouterModule.forRoot(
+      routes,
+      {onSameUrlNavigation: 'reload'}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
