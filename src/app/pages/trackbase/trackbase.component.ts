1160b27748bb0b6502cfec62c1ddee6eccc139cb
Hlakaza <nhlalucky9@gmail.com>
2018-10-14 17:11:58 +0200

    refactor(tables): addded dynamic trackbase tbles

--- src/app/pages/trackbase/trackbase.component.ts
+++ src/app/pages/trackbase/trackbase.component.ts
@@ -6,7 +6,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trackbase.component.scss']
 })
 export class TrackbaseComponent implements OnInit {
-
+  sList = [
+    {
+      'id': 1,
+       'sName': 'liquam tincidunt mauris eu risus',
+       'sType': 'Donsectetur adipisci',
+       'owner': 'Donsectetur adipisci',
+       'manager': 'Donsectetur adipisci',
+       'link': '',
+    },
+    {
+      'id': 2,
+       'sName': 'liquam tincidunt mauris eu risus',
+       'sType': 'Donsectetur adipisci',
+       'owner': 'Donsectetur adipisci',
+       'manager': 'Donsectetur adipisci',
+       'link': '',
+    },
+    {
+      'id': 3,
+       'sName': 'liquam tincidunt mauris eu risus',
+       'sType': 'Donsectetur adipisci',
+       'owner': 'Donsectetur adipisci',
+       'manager': 'Donsectetur adipisci',
+       'link': '',
+    },
+    {
+      'id': 4,
+       'sName': 'liquam tincidunt mauris eu risus',
+       'sType': 'Donsectetur adipisci',
+       'owner': 'Donsectetur adipisci',
+       'manager': 'Donsectetur adipisci',
+       'link': '',
+    },
+    {
+      'id': 5,
+       'sName': 'liquam tincidunt mauris eu risus',
+       'sType': 'Donsectetur adipisci',
+       'owner': 'Donsectetur adipisci',
+       'manager': 'Donsectetur adipisci',
+       'link': '',
+    },
+    {
+      'id': 6,
+       'sName': 'liquam tincidunt mauris eu risus',
+       'sType': 'Donsectetur adipisci',
+       'owner': 'Donsectetur adipisci',
+       'manager': 'Donsectetur adipisci',
+       'link': '',
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
